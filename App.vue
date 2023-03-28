4e2822f3942975af71e6e3411a2d1ccaf7dc2ae5
tifenn <tifennv@gmail.com>
2023-03-27 21:50:29 -0400

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,1 +1,20 @@
 
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.text = e.target.value
+    }
+  }
+}
+</script>
+
+<template>
+  <input :value="text" @input="onInput" placeholder="Type here">
+  <p>{{ text }}</p>
+</template>
