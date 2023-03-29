004399e4a06141d9d2d555b5a06569262d614bfa
tifenn <tifennv@gmail.com>
2023-03-28 21:24:25 -0400

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Recall Logo" src="./assets/recall.png">
+  <HealthRecall msg="Welcome to Health Canada Recall App"/>
+</template>
+
+<script>
+import HealthRecall from './components/HealthRecall.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HealthRecall
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
